4bbbbf03e6df7de3a46daa0f6005fff3e390d33e
Mariliis Jaago <mariliisjaago@hotmail.com>
2021-05-04 20:55:36 +0300

    Add project files.

---
+++ CalculationUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CalculationUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var estonianTemperatures = new Temperature();
+            var randomGenerator = new Random();
+
+            for (int i = 0; i < 100; ++i)
+            {
+                estonianTemperatures.Insert(randomGenerator.Next(1, 100));
+            }
+
+
+            Console.WriteLine($"Min is { estonianTemperatures.Min }");
+            Console.WriteLine($"Max is { estonianTemperatures.Max }");
+            Console.WriteLine($"Average is { estonianTemperatures.Average }");
+        }
+    }
+}
---
+++ CalculationUI/Temperature.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CalculationUI
+{
+    public class Temperature
+    {
+        private List<int> _temperatures = new List<int>();
+
+        public int Min { get; private set; }
+
+        public int Max { get; private set; }
+
+        public double Average { get; private set; }
+
+        public void Insert(int temperature)
+        {
+            // add guards?
+            if (temperature < 0 | temperature > int.MaxValue)
+            {
+                throw new Exception("Argument not in valid range (0 - maximum of int)");
+            }
+
+            AdjustMinAndMaxIfNeeded(temperature);
+
+            // add to list
+            _temperatures.Add(temperature);
+
+            // re-calculate average of list
+            Average = _temperatures.Average();
+        }
+
+        private void AdjustMinAndMaxIfNeeded(int temperature)
+        {
+            if (Min == 0)
+            {
+                Min = temperature;
+            }
+
+            // check if min
+            if (temperature < Min)
+            {
+                Min = temperature;
+            }
+
+            // check if max
+            if (temperature > Max)
+            {
+                Max = temperature;
+            }
+        }
+    }
+}
